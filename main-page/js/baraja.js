44099daecd23a74706decbb2c2cff3b3675c3ca8
Rosa <rhg1811@gmail.com>
2022-03-28 20:43:07 -0600

    Hola Estela

--- main-page/js/baraja.js
+++ main-page/js/baraja.js
@@ -8,4 +8,4 @@ img[2] = 'foto3.jpg" width="" height="" border=""';
 
 ran = Math.floor(3 * Math.random());
 
-document.write("<img src=""+img[ran]+"">");
+// document.write("<img src=""+img[ran]+"">");
