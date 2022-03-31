41d2cae96c6422b15feb90b27909591d9c17aae7
Estela <elestevillagonts@gmail.com>
2022-03-31 16:21:14 -0600

    cambios alerta

---
+++ main-page/js/alertas.js
@@ -0,0 +1,23 @@
+Swal.fire({
+    title: 'GANASTE :D',
+    text:  '¿Ya terminaste tus deberes?',
+    //html: '<img src="ganaste.jpg" alt="" />',
+    confirmButtonText: 'Salir del Juego',
+    //confirmButtonText: 'Volver a Jugar',
+    //width: '45%',
+    padding: '1rem',
+    background: '#000',
+    //grow: 'row',
+    backdrop: true,
+    //timer: 5000,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: true,
+    stopKeydownPropagation: false,
+
+    imageUrl: 'js/ganaste.jpg',
+    imageWidth: '100%',
+    imageHeight: '10vh',
+    imageAlt: 'bob esponja',
+
+})
